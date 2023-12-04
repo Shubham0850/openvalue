bad095b054f3b370cccadba18ddc67856c666f19
Kishor <yoyokunal156@gmail.com>
2023-12-05 01:02:26 +0530

    responsive why us

--- src/components/home/WhyUs.tsx
+++ src/components/home/WhyUs.tsx
@@ -5,33 +5,44 @@ const reasons = [
   {
     title: "Access to Broad Assets",
     description:
-      "Retail investors can gain access to alternative investments having sustainable returns backed by real assets ",
+      "Retail investors can gain access to alternative investments having sustainable returns backed by real assets ",
   },
   {
     title: "Fractionalization",
     description:
-      "Investors can own a fractional asset and be a owner in assets that typically are inaccessible without fractionalization. // reword ",
+      "Investors can own a fractional asset and be an owner in assets that typically are inaccessible without fractionalization. // reword ",
   },
   {
     title: "Transparency",
     description:
-      "All the transactions are recorded on the blockchain, making it fully transparent and immutable of ownership. ",
+      "All the transactions are recorded on the blockchain, making it fully transparent and immutable of ownership. ",
   },
   {
-    title: "Instant Settlement ",
+    title: "Instant Settlement ",
     description:
-      "Harness the power of DLT technologies to get instant settlements along with high liquidity and lower fees.  ",
+      "Harness the power of DLT technologies to get instant settlements along with high liquidity and lower fees. ",
   },
 ];
 
 function WhyUs() {
   return (
     <Container maxW={1300} p={{ base: "5vh 0px", md: "15vh 0px" }}>
-      <Box textAlign="center" mb={8}>
-        <Text as="h2" fontSize="48px" fontWeight="600" mb={6} color="#121212">
+      <Box textAlign="center" mb={{ base: 6, md: 8 }}>
+        <Text
+          as="h2"
+          fontSize={{ base: "32px", md: "48px" }}
+          fontWeight="600"
+          mb={{ base: 4, md: 6 }}
+          color="#121212"
+        >
           Why Us?
         </Text>
-        <Text fontSize="18px" fontWeight="300px" color="#616161" mb={28}>
+        <Text
+          fontSize={{ base: "16px", md: "18px" }}
+          fontWeight="300px"
+          color="#616161"
+          mb={{ base: 10, md: 28 }}
+        >
           Open value is a marketplace for top tier tokenized assets across
           various sectors
         </Text>
@@ -44,10 +55,15 @@ function WhyUs() {
               align="center"
               justify="center"
               textAlign="center"
-              width="48%"
+              width={{ base: "90%", md: "48%" }}
               margin="auto"
             >
-              <Text as="h3" fontSize="26px" fontWeight="600" mb={2}>
+              <Text
+                as="h3"
+                fontSize={{ base: "22px", md: "26px" }}
+                fontWeight="600"
+                mb={2}
+              >
                 {reason.title}
               </Text>
               <Text color="#616161">{reason.description}</Text>
